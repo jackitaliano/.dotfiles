306356d5cb9c119e47dfed79176b624560126c9d
jackitaliano <italiano.16@osu.edu>
2023-09-08 23:29:54 -0400

    add custom folder

---
+++ .oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,30 @@
+# zsh
+alias so="source ~/.zshrc"
+alias cl="clear"
+
+# vi
+alias v="nvim"
+alias vi="nvim"
+alias vim="nvim"
+alias nv="nvim"
+alias nvi="nvim"
+
+# tmux
+alias t="tmux"
+alias tl="tmux ls"
+alias ta="tmux attach"
+alias tk="tmux kill-session"
+alias tks="tmux kill-server"
+
+# git
+alias g="git"
+alias gs="git status"
+alias gd="git diff"
+alias gl="git log --graph --decorate --all"
+
+alias gcb="git checkout"
+alias gcnb="git checkout -b"
+alias gpnb="git push -u origin"
+alias gps="git push"
+alias gp="git pull"
+alias gaa="git add all"
