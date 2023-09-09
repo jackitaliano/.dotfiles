451541bd7767e7ea0bb3c11af360d162aa5c08aa
jackitaliano <italiano.16@osu.edu>
2023-09-08 23:29:54 -0400

    add custom folder

---
+++ .oh-my-zsh/custom/shortcuts.zsh
@@ -0,0 +1,16 @@
+projDir=~/projects
+noteDir=~/notes/2023
+confDir=~/.config
+dotsDir=~/.dotfiles
+scriptsDir=~/scripts
+reposDir=~/repos
+editorsDir=~/editors
+
+alias proj="cd $projDir"
+alias note="cd $noteDir"
+alias conf="cd $confDir"
+alias dots="cd $dotsDir"
+alias scripts="cd $scriptsDir"
+alias repos="cd $reposDir"
+alias editors="cd $ediorsDir"
+
