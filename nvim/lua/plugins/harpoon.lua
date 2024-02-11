9337c48ca3a827b91e2d441af5ca516ed17356a9
jackitaliano <italiano.16@osu.edu>
2024-02-11 15:42:36 -0500

    add quickfix

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -1,18 +1,19 @@
 return {
-  'ThePrimeagen/harpoon',
-  opts = function()
-    local mark = require('harpoon.mark')
-    local ui = require('harpoon.ui')
-
-    vim.keymap.set("n", "<leader>h", mark.add_file)
-    vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
-
-    vim.keymap.set("n", "<C-b>", function() ui.nav_file(1) end )
-    vim.keymap.set("n", "<C-n>", function() ui.nav_file(2) end )
-    vim.keymap.set("n", "<C-m>", function() ui.nav_file(3) end )
-
-  end,
-  dependencies = {
-  'nvim-lua/plenary.nvim',
-  },
+  -- 'ThePrimeagen/harpoon',
+  -- opts = function()
+  --   local mark = require('harpoon.mark')
+  --   local ui = require('harpoon.ui')
+  --   local term = require('harpoon.term')
+  --   local cmdUi = require('harpoon.cmd-ui')
+  --
+  --   vim.keymap.set("n", "<leader>h", mark.add_file)
+  --   vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+  --
+  --   vim.keymap.set("n", "<C-b>", function() ui.nav_file(1) end )
+  --   vim.keymap.set("n", "<C-m>", function() ui.nav_file(2) end )
+  --
+  -- end,
+  -- dependencies = {
+  -- 'nvim-lua/plenary.nvim',
+  -- },
 }
