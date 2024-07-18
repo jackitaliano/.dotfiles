379cf667e1bb13382c0c2ad326f0aaa25668fc74
jackitaliano <jitaliano.dev@gmail.com>
2024-07-18 15:40:16 -0400

    change to $HOME

--- scripts/finder_func.fish
+++ scripts/finder_func.fish
@@ -6,7 +6,7 @@ function finder
 	if [ $argc -eq 1 ]
 		set selected $1
 	else
-		set selected $(printf "$(find ~/projects ~/.dotfiles -mindepth 1 -maxdepth 1 -type d | grep -v '/\.[^/]*$')\n/Users/jitalian/.dotfiles" | \
+		set selected $(printf "$(find ~/projects ~/.dotfiles -mindepth 1 -maxdepth 1 -type d | grep -v '/\.[^/]*$')\n$HOME/.dotfiles" | \
 		fzf  --preview 'ls -AFh --color {} | bat --style=grid' --height=75% --margin=10%,5%,5%,10% --padding=2 --border=double --prompt='➤ ' --pointer='→' --preview-label='⟨ Directory Contents ⟩' --header='Finder' --header-first --color='fg:magenta'
 	)
 	end
