561d8393f18f2f5952ecdf6c13aebe59fdf8817a
jackitaliano <jitaliano.dev@gmail.com>
2025-06-04 21:57:03 -0500

    add alpha, lualine, surround

--- nvim/lua/plugins/surround.lua
+++ nvim/lua/plugins/surround.lua
@@ -1,14 +1,13 @@
--- autopairs
--- https://github.com/windwp/nvim-autopairs
-
 return {
-  'windwp/nvim-autopairs',
-  event = 'InsertEnter',
-  opts = {},
   -- {
   --   "altermo/ultimate-autopair.nvim",
   --   event = { "InsertEnter", "CmdlineEnter" },
   --   branch = "v0.6",
   --   opts = {},
   -- },
+  {
+    'kylechui/nvim-surround',
+    version = '*',
+    opts = {},
+  },
 }
