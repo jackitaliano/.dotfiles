e27e7cdf6a916e391f29dc3b4f51473dc862e10e
jackitaliano <jitaliano.dev@gmail.com>
2025-06-08 15:47:54 -0500

    make onedark default, fix colors

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -11,10 +11,13 @@ return {
         lualine = {
           transparent = false, -- lualine center bar transparency
         },
+        colors = {
+          dark_green = '#475a39',
+        },
         highlights = {
-          ['@lsp.type.comment'] = {
-            fg = '#559962',
-          },
+          ['comment'] = { fg = '$dark_green', fmt = 'none' },
+          ['@comment'] = { fg = '$dark_green', fmt = 'none' },
+          ['@lsp.type.comment'] = { fg = '$dark_green', fmt = 'none' },
         },
         diagnostics = {
           darker = true, -- darker colors for diagnostic
@@ -22,7 +25,7 @@ return {
           background = false, -- use background color for virtual text
         },
       }
-      -- require('onedark').load()
+      require('onedark').load()
     end,
     -- config = function()
     --   require('onedark').setup {
@@ -40,29 +43,7 @@ return {
     name = 'arctic',
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'arctic'
-    end,
-  },
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    enabled = true,
-    config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
-        styles = {
-          comments = { italic = false }, -- Disable italics in comments
-        },
-      }
-
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      -- vim.cmd.colorscheme 'tokyonight-night'
+      -- vim.cmd.colorscheme 'arctic'
     end,
   },
 }
